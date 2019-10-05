b9ecb3e556661fef63dc6d09f91d3447f3495d74
tomisin ajayi <48753117+TomisinAjayi@users.noreply.github.com>
2019-10-05 18:45:19 +0100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -148,8 +148,7 @@ function Login(props) {
                     <h4> TristonFinApp gives best services to our customers. </h4>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                       
                     </p>
                     <Button
                         className="inverted-white get-started"
